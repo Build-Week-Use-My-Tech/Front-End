5c8c6f09a292487ebcebc61896fd526549d4f0d2
Funmi7 <talabifunmilayo@gmail.com>
2019-09-24 13:51:54 +0100

    Cretaed an itemslist, itemscard and additem components

---
+++ rent-my-tech-fe/src/components/ItemsCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ItemsCard () {
+    return(
+        <div></div>
+    )
+}
+
+export default ItemsCard;
---
+++ rent-my-tech-fe/src/components/ItemsList.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+
+function ItemsList() {
+    const [ItemsList, setItemsList] = useState([]);
+
+    useEffect(() => {
+        
+    })
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ItemsList;
