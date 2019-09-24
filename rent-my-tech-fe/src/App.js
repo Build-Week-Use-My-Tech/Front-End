314bf805a3145a51dc1322f3e6214c81d52bda66
Dale Pancho <dalepancho7@gmail.com>
2019-09-24 13:23:07 -0700

    nav confusion

--- rent-my-tech-fe/src/App.js
+++ rent-my-tech-fe/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navigation from "./components/nav"
+import Navigation from "./components/Nav"
 
 function App() {
   return (
--- rent-my-tech-fe/src/components/Nav.js
+++ rent-my-tech-fe/src/components/Nav.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav, Navbar, Link, Form, FormControl, Button} from "react-bootstrap";
 
 const Navigation = () => {
     return(
         <div className="nav-bar">
-            <div className="logo">
-                {/* enter logo here */}
-            </div>
-            <div className="navLinks">
-                <Link to="/">Post your Tech</Link>
-            </div>
-            <div className="navLinks">
-                <Link to="/">Sign Up</Link>
-            </div>
-            <div className="navLinks">
-                <Link to="/">Log In</Link>
-            </div>
+              <Navbar bg="primary" variant="dark">
+              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/Login">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav>
+              <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-light">Search</Button>
+              </Form>
+            </Navbar>
         </div>
     );
 };
