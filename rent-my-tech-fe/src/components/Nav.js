490c8d624d444797c1c8cf392f05417fdb96f043
Julie <julieantonio38@gmail.com>
2019-09-25 15:13:45 -0400

    set up routing

--- rent-my-tech-fe/src/components/Nav.js
+++ rent-my-tech-fe/src/components/Nav.js
@@ -1,21 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Login from './Login.js'
+import SignUp from './SignUp.js'
+import AddItem from './AddItem.js'
+import Dashboard from './Dashboard.js'
+import ItemsList from './ItemsList'
+import PrivateRoute from './PrivateRoute'
+import { Link, Route, Switch } from "react-router-dom";
 
 const Navigation = () => {
     return(
         <div className="nav-bar">
             <div className="logo">
-                {/* enter logo here */}
+                <Link to='/'><img src=' {/* enter logo here */}' /> </Link>
             </div>
             <div className="navLinks">
-                <Link to="/">Post your Tech</Link>
+                <Link to="/additem">Post your Tech</Link>
             </div>
             <div className="navLinks">
-                <Link to="/">Sign Up</Link>
+                <Link to="/signup">Sign Up</Link>
             </div>
             <div className="navLinks">
-                <Link to="/">Log In</Link>
+                <Link to="/login">Log In</Link>
             </div>
+            <Switch>
+                <Route exact path='/' component={ItemsList} />
+                <Route path='/login' component={Login} />
+                <Route path='/signup' component={SignUp} />
+                <PrivateRoute path='/dashboard' component={Dashboard} />
+                <PrivateRoute path='/additem' component={AddItem} />
+                </Switch>   
         </div>
     );
 };
